3bb07c2ddc8b08d75fda9959a497c2a93d33466f
Mikigames0009 <virsli124@gmail.com>
2023-12-23 11:47:24 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,50 @@ using namespace std;
 int main(int argc, char** argv) {
     if (argc > 2)
     {
+        cout << endl;
         cout << "More than 2 arguments!";
+        cout << endl;
         exit(-1);
     }
 
     if (argc < 2)
     {
+        cout << endl;
         cout << "Less than 2 arguments!";
+        cout << endl;
         exit(-1);
     }
 
     string filename = argv[1];
-
     fstream file(filename);
 
-    if (!file) {
-        cout << "Failed to open!";
-        exit(-1);
-    }
+    string help;
+    help = "Help:";
 
-    cout << endl;
+    if (filename == "--help") {
+        cout << endl;
+        cout << help;
+        cout << endl;
+    }
+    else if (!file) {
+        cout << "Failed to open!";
+        file.close();
+        exit(-1);
+        
+    }
+    else
+    {
+        cout << endl;
+    }
 
     string line;
 
-    while (getline(file, line)) {
+     while (getline(file, line)) {
 	    cout << line << endl;
     }
-
+    if (file) {
     cout << endl;
+    }
 
     file.close();
  
